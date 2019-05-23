bb77027de35a6faba2da0ec6b59d1bf9ec08739c
Jonah Quist <jonah@DESKTOP-LG0F1MV.localdomain>
2019-05-23 14:31:44 -0600

    setup fb utilities

--- framebuffer.h
+++ framebuffer.h
@@ -1,3 +1,4 @@
+#define RGBA_SZ sizeof(int) 
 
 typedef struct {
 	void* fb;
@@ -11,3 +12,8 @@ typedef struct {
 } point_t;
 
 int draw_aaline(framebuffer_t* fb, unsigned color, point_t* p1, point_t* p2);
+
+framebuffer_t* framebuffer_init(int w, int h);
+unsigned framebuffer_px(framebuffer_t* fb, int x, int y);
+void framebuffer_repr(framebuffer_t* fb);
+void framebuffer_dump(framebuffer_t* fb);
--- main.c
+++ main.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "framebuffer.h"
+
+framebuffer_t* framebuffer_init(int w, int h) {
+	int fb_sz = w * h * RGBA_SZ;
+	void* fb_frame = malloc(fb_sz);
+	memset(fb_frame, 0, fb_sz);
+	framebuffer_t* fb = malloc(sizeof(framebuffer_t));
+	fb->fb = fb_frame;
+	fb->width = w;
+	fb->height = h;
+	return fb;
+}
+
+unsigned framebuffer_px(framebuffer_t* fb, int x, int y) {
+	void* fb_frame = fb->fb;
+	fb_frame += RGBA_SZ * fb->width * y;
+	fb_frame += RGBA_SZ + x;
+	return *((unsigned*) fb_frame);
+}
+
+void framebuffer_repr(framebuffer_t* fb) {
+	printf("%dx%d\n", fb->width, fb->height);
+	for(int i = 0; i < fb->height; i++) {
+		for(int j = 0; j < fb->width; j++) {
+			putchar(framebuffer_px(fb, j, i));
+		}
+		putchar('\n');
+	}
+}
 
 int main() {
-
+	framebuffer_t* fb = framebuffer_init(10, 15);
+	framebuffer_repr(fb);
+	return 0;
 }
